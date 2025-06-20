a439521249e521b0400c649bff3f8aa6d6dd719f
Sipho Maswanganye <38434202+M60-live@users.noreply.github.com>
2025-06-20 08:18:53 +0200

    Contact info update

--- resources/views/component/service_contact.blade.php
+++ resources/views/component/service_contact.blade.php
@@ -1,9 +1,9 @@
 <div class="side-contact py-40 px-30 mt-30 bg-gray">
     <h3 class="position-relative va-lb-line-w50-h2-primary pb-15 mb-30 color-secondery">Contact Us</h3>
     <ul class="color-dark icon-primary link-list-b-15">
-        <li><span class="mr-20"><i class="fa fa-phone" aria-hidden="true"></i></span> +61 600 800 900</li>
+        <li><span class="mr-20"><i class="fa fa-phone" aria-hidden="true"></i></span> (012) 345 657 900</li>
         <li><span class="mr-20"><i class="fa fa-envelope" aria-hidden="true"></i></span> info@sedimaadvisoryandfinance.co.za</li>
-        <li class="m-0"><span class="mr-20"><i class="fa fa-map-marker" aria-hidden="true"></i></span>40 Towerhill Avenue, Melbourne, Australia.</li>
+        <li class="m-0"><span class="mr-20"><i class="fa fa-map-marker" aria-hidden="true"></i></span>379 San Isidro Street, Silverwood Estate, Centurion</li>
     </ul>
 </div>
 <!--<a class="btn btn-secondery mt-30 w-100" href="#">Live Chat</a>-->
