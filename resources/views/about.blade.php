2d6b909e83d7435a93e8389d647013d9ea31e313
Sipho Maswanganye <38434202+M60-live@users.noreply.github.com>
2025-07-04 10:01:07 +0200

    About Page Changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,15 +8,15 @@
             <div class="banner-text text-center">
                 <div class="row">
                     <div class="col-md-12 col-lg-12">
-                        <h1 class="position-relative va-c-line-w50-h1-primary pb-15 mb-20 color-white">About</h1>
+                        <h1 class="position-relative va-c-line-w50-h1-primary pb-15 mb-20 color-white">My Profile</h1>
                         <nav class="breadcrumb-one d-table m-auto bg-white px-30" aria-label="breadcrumb">
                             <ol class="breadcrumb bg-transparent mb-0">
                                 <li class="breadcrumb-item"><a href="/">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">About</li>
+                                <li class="breadcrumb-item active" aria-current="page">My Profile</li>
                             </ol>
                         </nav>
                         <div class="text-area color-white w-50 lg-w-80 md-w-100 d-table mx-auto mt-20">
-                            <p>At Sedima Advisory and Finance, we are committed to helping small and medium businesses realize their full potential through expert financial management, clear insights, and personalized support. Our mission is to empower you with the tools and knowledge needed to build a profitable, sustainable business.</p>
+{{--                            <p>At Sedima Advisory and Finance, we are committed to helping small and medium businesses realize their full potential through expert financial management, clear insights, and personalized support. Our mission is to empower you with the tools and knowledge needed to build a profitable, sustainable business.</p>--}}
                         </div>
                     </div>
                 </div>
@@ -28,7 +28,7 @@
 
     <!-- Start Team Style One
 	====================================================-->
-    <section>
+    {{--<section>
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-lg-12">
@@ -69,7 +69,45 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section>--}}
     <!-- End Team Style One
+    ====================================================-->
+
+    <!-- Start About Us
+	====================================================-->
+    <section id="about" class="about-us-3 position-relative bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-12 col-xl-5">
+                    <div class="about-img-2 wow animated slideInRight animated">
+                        <img src="images/about/1.jpg" alt="image">
+                        <div class="about-area-2 bg-secondery d-flex color-white p-40 wow animated slideInUp animated">
+                            <p>If you’re looking for someone who understands both the structure of business and the soul behind it, I’d love to connect.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-12 col-xl-7">
+                    <div class="position-relative">
+                        <div class="inner-title mb-30">
+                            <h2 class="color-secondery xl-mt-30 wow animated slideInUp animated">Mpho Lekokotla, founder and director</h2>
+                        </div>
+                        <div class="text-area mb-30 wow animated slideInUp animated">
+                            <p class="mb-15">
+                                I hold a Postgraduate Diploma in Cost and Management Accounting and I’m a registered CIMA member, which means I’m trained to look beyond the numbers - analyzing risks, identifying opportunities, and guiding businesses toward sustainable profitability.<br><br>
+                                With experience in risk, pricing, and financial strategy, I’ve worked on projects that involve everything from vehicle cost restructuring to advising on business performance. But beyond the numbers, what drives me is seeing people gain confidence in their vision and take control of their business journey.<br><br>
+                                Sedima was born from my desire to merge technical expertise with a heart for service. I wanted to create more than just an accounting firm—I wanted to build a space where faith, purpose, and professional excellence meet.<br><br>
+                                When I’m not working with clients, I create podcast content that blends real-life experiences with Biblical truth - helping others grow spiritually, personally, and professionally.
+                            </p>
+                            <p></p>
+                        </div>
+                        <div class="about-area-2 bg-secondery d-flex color-white p-40 wow animated slideInUp animated">
+                            <p>I’m passionate about helping small and medium businesses unlock their full potential - because I believe growth should be accessible to every entrepreneur, not just those with big budgets.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About Us
     ====================================================-->
 @endsection
